278cee62f846fbd16233ea80998a636a99c96b6f
juanf97 <juanvpipec1227@gmail.com>
2021-03-25 15:07:42 -0500

    actual

--- javascript/SopadeLetras.js
+++ javascript/SopadeLetras.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".head").hover(function(){
-        $(". head").addClass('resaltar');
+        $(".head").addClass('resaltar');
     });
     $(".head").mouseleave(function(){
         $(".head").removeClass('resaltar');
