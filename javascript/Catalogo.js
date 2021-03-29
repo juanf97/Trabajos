1580948b5de8a9fde5700c607cb1759832f115e7
juanf97 <juanvpipec1227@gmail.com>
2021-03-29 18:17:33 -0500

    entregable

--- javascript/Catalogo.js
+++ javascript/Catalogo.js
@@ -4,8 +4,8 @@ function add(producto, precio){
     console.log(producto,  precio);
     productos.push(producto);
     total= total + precio;
-    document.getElementById("tiendap").innerHTML = 'valor a pagar $${total}';
-}'
+    document.getElementById("tiendap").innerHTML ='valor a pagar $${total}'
+}
 function pay(){
     window.alert(productos.join(", \n")); 
 }
