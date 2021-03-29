b52b2174a4a65cd9dbb401a7a7b38a20ad5535a4
juanf97 <juanvpipec1227@gmail.com>
2021-03-29 17:54:51 -0500

    calificlable

---
+++ javascript/Catalogo.js
@@ -0,0 +1,11 @@
+let productos  =[]
+let total =0;
+function add(producto, precio){
+    console.log(producto,  precio);
+    productos.push(producto);
+    total= total + precio;
+    document.getElementById("tiendap").innerHTML = 'valor a pagar $${total}';
+}'
+function pay(){
+    window.alert(productos.join(", \n")); 
+}
