aceaa9e27f3f72deab04f6bf48610e96a3b67500
juanf97 <juanvpipec1227@gmail.com>
2021-05-01 16:37:35 -0500

    nuevo

--- SopadeLetras/SopadeLetras.js
+++ SopadeLetras/SopadeLetras.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
     $(".head").hover(function(){
         $(".head").addClass('resaltar');
     });
-    $(".head").mouseleave(function(){
-        $(".head").removeClass('resaltar');
-    }); 
 });
 
 
