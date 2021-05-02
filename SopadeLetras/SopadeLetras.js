2b09c60fdbd5e878206b1412bdcd99f4cb8b4be8
juanf97 <juanvpipec1227@gmail.com>
2021-05-02 14:45:06 -0500

    consultas

--- SopadeLetras/SopadeLetras.js
+++ SopadeLetras/SopadeLetras.js
@@ -1,8 +1,18 @@
-$(document).ready(function(){
-    $(".head").hover(function(){
-        $(".head").addClass('resaltar');
+/*function resaltar(){
+   var c1= document.getElementById("c1");
+   c1.className ="resaltar";
+    
+}*/
+
+$(document).ready(function () {
+    $(".header").hover(function () {
+        $(".header").addClass("resaltar");
     });
+    $(document).mouseleave(function () {
+        $(".header").removeClass(function () {
+            
+        });
+    });
+
 });
-
-
 
