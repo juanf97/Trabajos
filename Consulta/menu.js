8b80723b3c3d98a5e62e7705548d133abe2ad411
juanf97 <juanvpipec1227@gmail.com>
2021-05-05 16:50:55 -0500

    final

---
+++ Consulta/menu.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.titulo');
+ScrollReveal().reveal('.informacion', {delay: 500});
+ScrollReveal().reveal('.titulo1', {delay: 500});
+ScrollReveal().reveal('.informacion1', {delay: 500});
--- SopadeLetras/SopadeLetras.js
+++ SopadeLetras/SopadeLetras.js
@@ -1,18 +1,25 @@
-/*function resaltar(){
-   var c1= document.getElementById("c1");
-   c1.className ="resaltar";
-    
-}*/
+//Funciones
+function addEventListenerListResaltar(list) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener('mouseover', function(){list.forEach(function(e){e.className = "caja resaltar"})}, false);
+        list[i].addEventListener('mouseleave', function(){list.forEach(function(e){e.className = "caja"})}, false);
+    }
+}
 
-$(document).ready(function () {
-    $(".header").hover(function () {
-        $(".header").addClass("resaltar");
-    });
-    $(document).mouseleave(function () {
-        $(".header").removeClass(function () {
-            
-        });
-    });
+function resolveList(list){
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].className = "caja resaltar"
+        list[i].removeEventListener('mouseover',function(){console.log("Removed")})
+        list[i].removeEventListener('mouseleave',function(){console.log("Removed")})
+    }
+}
 
-});
+//Busqueda de elementos
+var elementDom = document.querySelectorAll(".caja .head .header")
 
+//Click Resolver
+btnResolver.addEventListener('click',function(){
+   resolveList(elementHeader)
+})
+
+addEventListenerListResaltar(elementHeader)
