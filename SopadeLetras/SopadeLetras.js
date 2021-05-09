342d528e15b64e784d4dd9436b32db7ab9ce8943
juanf97 <juanvpipec1227@gmail.com>
2021-05-09 13:26:30 -0500

    inicia

--- SopadeLetras/SopadeLetras.js
+++ SopadeLetras/SopadeLetras.js
@@ -13,13 +13,57 @@ function resolveList(list){
         list[i].removeEventListener('mouseleave',function(){console.log("Removed")})
     }
 }
+var btnResolver = document.getElementById("btnResolver");
 
-//Busqueda de elementos
-var elementDom = document.querySelectorAll(".caja .head .header")
-
-//Click Resolver
 btnResolver.addEventListener('click',function(){
-   resolveList(elementHeader)
+    resolveList(elementHeader)
+    resolveList(elementHead)
+    resolveList(elementDiv)
+    resolveList(elementNav)
+    resolveList(elementAddress)
+    resolveList(elementNoscript)
+    resolveList(elementTitle)
+    resolveList(elementMain)
+    resolveList(elementHr)
+    resolveList(elementSection)
+    resolveList(elementScript)
+    resolveList(elementStyle)
+    resolveList(elementHr)
+    resolveList(elementHr)
+   
 })
 
+//Busqueda de elementos
+var elementHeader = document.querySelectorAll(".header")
+var elementHead = document.querySelectorAll(".head")
+var elementDiv = document.querySelectorAll(".div")
+var elementNav = document.querySelectorAll(".nav")
+var elementAddress = document.querySelectorAll(".address")
+var elementNoscript = document.querySelectorAll(".noscript")
+var elementTitle = document.querySelectorAll(".title")
+var elementMain = document.querySelectorAll(".main")
+var elementHr = document.querySelectorAll(".hr")
+var elementSection = document.querySelectorAll(".section")
+var elementScript = document.querySelectorAll(".script")
+var elementStyle = document.querySelectorAll(".style")
+var elementScript = document.querySelectorAll(".script")
+
+
+
+
+//Eventos para Resaltar
+
 addEventListenerListResaltar(elementHeader)
+addEventListenerListResaltar(elementHead)
+addEventListenerListResaltar(elementDiv)
+addEventListenerListResaltar(elementNav)
+addEventListenerListResaltar(elementAddress)
+addEventListenerListResaltar(elementNoscript)
+addEventListenerListResaltar(elementTitle)
+addEventListenerListResaltar(elementMain)
+addEventListenerListResaltar(elementHr)
+addEventListenerListResaltar(elementSection)
+addEventListenerListResaltar(elementScript)
+addEventListenerListResaltar(elementStyle)
+addEventListenerListResaltar(elementHead)
+addEventListenerListResaltar(elementHead)
